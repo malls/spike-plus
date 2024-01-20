af45453855d3f0856c3d7af7133f1f1a6387f6a8
Forrest Almasi <_@forrestalmasi.com>
2024-01-20 17:02:32 -0600

    fix flexbox stuff

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,10 +2,9 @@ import './AppHeader.css';
 function AppHeader () {
 	return (
 		<header>
-
 			<div className="menu">
-				<a href="/about"><div className="menu-item">About</div></a>
 				<a href="/work"><div className="menu-item">Work</div></a>
+				<a href="/about"><div className="menu-item">About</div></a>
 				<a href="/contact"><div className="menu-item">Contact</div></a>
 			</div>
 		</header>
