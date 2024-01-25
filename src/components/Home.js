0ec40b10a75665628690bd78b5ffe2c84605a2e4
Forrest Almasi <_@forrestalmasi.com>
2024-01-25 10:29:04 -0600

    start footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,23 @@
 import SiteFooter from './SiteFooter.js';
 import Spike from './Spike.js';
+import './Home.css';
+import { useLoaderData } from 'react-router-dom';
+
 
 export default function Home () { 
+
+	const { data } = useLoaderData();
+
 	return (
 		<div>
 			<Spike></Spike>
 
-			home
-			<br></br>
-			fdsafdsaf
-			<br></br>
-			fdsafdsaffdsa
-			<br></br>
-			fdsafdsafbr	
-			<br></br>
+
+			<div className="home-container">
+				{data.map((work) => <img src={work.img} alt={work.alt} className='home-image'/>)}
+			</div>
+
+
 			<SiteFooter></SiteFooter>
 		</div>
 	)
--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -2,7 +2,14 @@ import './SiteFooter.css'
 
 function SiteFooter () {
 	return (
-		<footer>footer</footer>
+		<footer>
+			<a href='mailto:info@spikeplus.studio'>info@spikeplus.studio</a>
+
+			<span>© 2024 Spike Plus</span>
+
+			<a href='https://www.instagram.com/spikeplus.studio/' target='_blank' rel='noreferrer'>instagram</a>
+			<a href='https://www.linkedin.com/spikeplus.studio/' target='_blank' rel='noreferrer'>LinkedIn</a>
+		</footer>
 	)
 }
 
