89405c876b222f698345135bd2b98ea6248eb6f8
Forrest Almasi <_@forrestalmasi.com>
2024-01-24 19:48:44 -0600

    ipad view fixes and contact stuff

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,25 @@
+import './About.css';
 
 export default function Contact () { 
 	return (
-		<div>contact</div>
+		<div className="info">
+			<div className='info-label'>Contact</div>
+			<div className="info-text">
+				<a href="mailto:Info@spikeplus.studio">Info@spikeplus.studio </a>
+			</div>
+			<div className='info-spacer'></div>
+
+			<div className='info-label'>New business</div>
+			<div className="info-text">
+				Print & Contact
+				<br/>
+				Dean Snyder
+				<br/>
+				Info@printandcontact.com
+				<br/>
+				212 436 8845
+			</div>
+			<div className='info-spacer'></div>
+		</div>
 	)
 }
