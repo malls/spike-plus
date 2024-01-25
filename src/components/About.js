852a5c3735acdad048b482167b739aa68ca8c59c
Forrest Almasi <_@forrestalmasi.com>
2024-01-24 19:38:23 -0600

    white pages styling

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,9 @@ import './About.css';
 
 export default function About () { 
 	return (
-		<div className="about">
-			<div className='about-label'>About Spike Plus</div>
-			<div className="about-text">
+		<div className="info">
+			<div className='info-label'>About Spike Plus</div>
+			<div className="info-text">
 				<p>
 					Spike Plus unites creative, strategic, and technological artists from around the world to establish a one-of-a-kind design studio. A network of craftspeople who collaborate, forming a rich incubator for unique visions, projects and products to originate.
 				</p>
@@ -19,97 +19,71 @@ export default function About () {
 					Uniquely strategic 360 programs propel new opportunities and creative discoveries to establish exceptional brands and elevate clients to the forefront of cultural influence.
 				</p>
 			</div>
-			<div className='about-spacer'></div>
-			<div className='about-label'>Services</div>
-			<div className='about-text about-column-container'>
-				<div className='about-column'>
-					<p>
-						01. Branding
-					</p>
-					<p>
-						Purpose and Brand Strategy
-					</p>
-					<p>
-						Brand Architecture
-					</p>
-					<p>
-						Brand Design
-					</p>
-					<p>
-						Custom Typography & Typefaces
-					</p>
-					<p>
-						Naming & Verbal Identity
-					</p>
-					<p>
-						Visual Image Direction
-					</p>
-					<p>
-						Integrated Campaigns & Activations
-					</p>
-					<p>
-						Product & Packaging Design
-					</p>
-					<p>
-						Social Media & Content
-					</p>
-					<p>
-						OOH and Disruptive Media
-					</p>
+			<div className='info-spacer'></div>
+			<div className='info-label'>Services</div>
+			<div className='info-text info-column-container'>
+				<div className='info-column'>
+					01. Branding
+					<br/>
+					Purpose and Brand Strategy
+					<br/>
+					Brand Architecture
+					<br/>
+					Brand Design
+					<br/>
+					Custom Typography & Typefaces
+					<br/>
+					Naming & Verbal Identity
+					<br/>
+					Visual Image Direction
+					<br/>
+					Integrated Campaigns & Activations
+					<br/>
+					Product & Packaging Design
+					<br/>
+					Social Media & Content
+					<br/>
+					OOH and Disruptive Media
+					<br/>
 				</div>
-				<div className='about-column'>
-					<p>
-						02. Technology
-					</p>
-					<p>
-						Games design and production
-					</p>
-					<p>
-						CGI
-					</p>
-					<p>
-						Web3 & Virtual Commerce
-					</p>
-					<p>
-						Web and app design
-					</p>
-					<p>
-						UX and UI design
-					</p>
-					<p>
-						Service & Experience Design
-					</p>
-					<p>
-						e-commerce
-					</p>
+				<div className='info-column'>
+					02. Technology
+					<br/>
+					Games design and production
+					<br/>
+					CGI
+					<br/>
+					Web3 & Virtual Commerce
+					<br/>
+					Web and app design
+					<br/>
+					UX and UI design
+					<br/>
+					Service & Experience Design
+					<br/>
+					e-commerce
+					<br/>
 				</div>
-				<div className='about-column'>
-					<p>
-						03. Production & Talent
-					</p>
-					<p>
-						Project Production
-					</p>
-					<p>
-						Rights and usage
-					</p>
-					<p>
-						Casting
-					</p>
-					<p>
-						Talent and Celebrity negotiations
-					</p>
-					<p>
-						Brand collaborations
-					</p>
-					<p>
-						Influencer management
-					</p>
+				<div className='info-column'>
+					03. Production & Talent
+					<br/>
+					Project Production
+					<br/>
+					Rights and usage
+					<br/>
+					Casting
+					<br/>
+					Talent and Celebrity negotiations
+					<br/>
+					Brand collaborations
+					<br/>
+					Influencer management
+					<br/>
 				</div>
 
-				<div className='about-spacer'></div>
 
 			</div>
+			<div className='info-spacer'></div>
 
 		</div>
 	)
