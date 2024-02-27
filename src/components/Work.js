c12719809011b3ea0ff0c87076af0754264445dc
Forrest Almasi <_@forrestalmasi.com>
2024-02-27 15:18:07 -0600

    grid stuff for work description

--- src/components/Work.js
+++ src/components/Work.js
@@ -14,8 +14,13 @@ export default function Work () {
 		<div className="work-container">
 			<Hero heroData={heroData1}></Hero>
 			<div className="work-text-container">
-				Description: {data.description}
-				Services: {data.services.map((service) => <div>{service}</div>)}
+				<div className="work-description-title">Project Description</div>
+				<div className="work-description">{data.description}</div>
+				<div className="work-services">
+					Services
+					<br/>
+					{data.services.map((service) => <div>{service}</div>)}
+				</div>
 			</div>
 			{data.images.slice(1).map((image) => <Hero heroData={image}></Hero>)}
 		</div>
