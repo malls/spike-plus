06e3ba452d17f07a96dfb0599825096e33c60da9
Forrest Almasi <_@forrestalmasi.com>
2024-03-06 21:06:13 -0500

    fix mobile contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,33 @@
 import './About.css';
+import SiteFooter from './SiteFooter';
 
 export default function Contact () { 
 	return (
-		<div className="info">
-			<div className='info-label'>Contact</div>
-			<div className="info-text info-text-big">
-				<a href="mailto:Info@spikeplus.studio">Info@spikeplus.studio </a>
-			</div>
-			<div className='info-spacer'></div>
-
-			<div className='info-label'>New business</div>
-			<div className="info-text">
-				<div className='info-column'>
-					Print & Contact
-					<br/>
-					Dean Snyder
-					<br/>
-					<a href="mailto:Info@printandcontact.com">Info@printandcontact.com</a>
-					<br/>
-					<a href="tel:+1212-436-8845">212 436 8845</a>
+		<div className='contact-container'>
+			<div className="info">
+				<div className='info-label'>Contact</div>
+				<div className="info-text">
+					<a href="mailto:Info@spikeplus.studio">Info@spikeplus.studio </a>
 				</div>
+				<div className='info-spacer'></div>
+
+				<div className='info-label'>New business</div>
+				<div className="info-text">
+					<div className='info-column'>
+						Print & Contact
+						<br/>
+						Dean Snyder
+						<br/>
+						<a href="mailto:Info@printandcontact.com">Info@printandcontact.com</a>
+						<br/>
+						<a href="tel:+1212-436-8845">212 436 8845</a>
+					</div>
+				</div>
+
+				<div className='info-spacer'></div>
 			</div>
-			<div className='info-spacer'></div>
+			<SiteFooter></SiteFooter>
 		</div>
+
 	)
 }
