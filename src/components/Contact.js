bf232abd4e3d75db98d217f69e15c104a71d19d4
Forrest Almasi <_@forrestalmasi.com>
2024-03-07 22:09:57 -0500

    footer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,7 @@ import SiteFooter from './SiteFooter';
 
 export default function Contact () { 
 	return (
-		<div className='contact-container'>
-			<div className="info">
+		<div className='info'>
 				<div className='info-label'>Contact</div>
 				<div className="info-text">
 					<a href="mailto:Info@spikeplus.studio">Info@spikeplus.studio </a>
@@ -23,9 +22,14 @@ export default function Contact () {
 						<a href="tel:+1212-436-8845">212 436 8845</a>
 					</div>
 				</div>
-
 				<div className='info-spacer'></div>
-			</div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+
 			<SiteFooter background={'white'}></SiteFooter>
 		</div>
 
