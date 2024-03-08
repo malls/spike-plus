a66749bb2a8b2eac067c668ac61435c057aa611e
Forrest Almasi <_@forrestalmasi.com>
2024-03-07 21:53:32 -0500

    footer work

--- src/components/SiteFooter.js
+++ src/components/SiteFooter.js
@@ -8,16 +8,16 @@ function SiteFooter ({background}) {
 		<footer className={backgroundClass}>
 
 			<div className='footer-column'>
-				<a href='mailto:info@spikeplus.studio'><div className='footer-email'>info@spikeplus.studio</div></a>
-				<div className='footer-spacer'>
+				<a href='mailto:info@spikeplus.studio'><div className='footer-text hide-mobile'>info@spikeplus.studio</div></a>
+				<div className='footer-text hide-mobile'>
 					<br/>
 				</div>
-				<div>© 2024 Spike Plus Studio</div>
+				<div className='footer-text'>© 2024 Spike Plus Studio</div>
 			</div>
 			<div className='footer-column'>
-				<a href='https://www.instagram.com/spikeplus.studio/' target='_blank' rel='noreferrer'><div>Instagram</div></a>
-				<a href='https://www.linkedin.com/spikeplus.studio/' target='_blank' rel='noreferrer'><div>LinkedIn</div></a>
-				<div className='footer-spacer'>
+				<a href='https://www.instagram.com/spikeplus.studio/' target='_blank' rel='noreferrer'><div className='footer-text'>Instagram</div></a>
+				<a href='https://www.linkedin.com/spikeplus.studio/' target='_blank' rel='noreferrer'><div className='footer-text'>LinkedIn</div></a>
+				<div className='footer-text'>
 					<br/>
 				</div>
 			</div>
