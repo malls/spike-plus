c33cfe680fc6d61ab03a734e41614a56ad87b8bc
Forrest Almasi <_@forrestalmasi.com>
2024-04-03 18:01:30 -0400

    more projects on client page

--- src/components/Work.js
+++ src/components/Work.js
@@ -6,22 +6,35 @@ import './Work.css';
 export default function Work () {
 	const { data } = useLoaderData();
 
+	const sections = data.projects.map((project) => {
+		return <div className='work-project-container'>
+
+			<div className='work-project-title'>
+				{project.title}
+			</div>
+
+			<Hero heroData={project.content[0]}></Hero>
+				<div className="work-text-container">
+					<div className='work-client-tag'>{data.client}</div>
+					<div className='work-description'>{project.description}</div>
+					<div className='mobile-only'></div>
+					<div className='work-services'>
+						Services
+						<br/>
+						{project.services.map((service) => <div key={service}>{service}</div>)}
+					</div>
+				</div>
+				<div className="work-work-container">
+					{project.content.slice(1).map((media) => <Hero heroData={media} key={media.url}></Hero>)}
+				</div>
+		</div>
+	});
+
+	console.log({data, sections});
+
 	return (
 		<div className="work-container">
-			<Hero heroData={data.content[0]}></Hero>
-			<div className="work-text-container">
-				<div className='work-client-tag'>{data.client}</div>
-				<div className='work-description'>{data.description}</div>
-				<div className='mobile-only'></div>
-				<div className='work-services'>
-					Services
-					<br/>
-					{data.services.map((service) => <div key={service}>{service}</div>)}
-				</div>
-			</div>
-			<div className="work-work-container">
-				{data.content.slice(1).map((media) => <Hero heroData={media} key={media.url}></Hero>)}
-			</div>
+			{sections}
 			<WorkFooter></WorkFooter>
 		</div>
   )
--- src/index.js
+++ src/index.js
@@ -128,31 +128,61 @@ const router = createBrowserRouter([
 					return { 
 						data: {
 							id: params.id,
-							services: [
-								'service1',
-								'service2',
-								'service3',
-								'service4',
-							],
-							title: 'Project title',
 							client: 'Ralph Lauren Polo',
-							description: 'Description blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blahDescription blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blahDescription blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blahDescription blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blahDescription blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
-							content: [
+							projects: [
+								{
+									services: [
+										'service1',
+										'service2',
+										'service3',
+										'service4',
+									],
+									title: 'Spring 2023',
+									description: 'Description blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blahDescription blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blahDescription blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blahDescription blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blahDescription blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah',
+									content: [
+		
+										{ url: '/vince/VinceHoliday_Video_2.mp4', alt: '', title: 'title', photographer: '', type: 'video'},
+										{ url: '/vince/Holiday2024_1.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_2.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_3.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_4.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_5.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_6.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_7.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_8.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_9.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/df67fb09-a602-4dd1-966f-e27623f76aab (1).mp4' , alt: '', title: 'title', photographer: '', type: 'video'},
+										{ url: '/vince/f2fbb8b4-940c-4b6c-9b7e-47cec6094ce4 (1).mp4', alt: '', title: 'title', photographer: '', type: 'video'},
+									]
+								},
+								{
+								services: [
+										'service1',
+										'service2',
+										'service3',
+										'service4',
+									],
+									description: 'Chenged this to polo so the title looks longer',
+									title: 'Holiday 2024',
 
-								{ url: '/vince/VinceHoliday_Video_2.mp4', alt: '', title: 'title', photographer: '', type: 'video'},
-								{ url: '/vince/Holiday2024_1.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
-								{ url: '/vince/Holiday2024_2.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
-								{ url: '/vince/Holiday2024_3.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
-								{ url: '/vince/Holiday2024_4.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
-								{ url: '/vince/Holiday2024_5.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
-								{ url: '/vince/Holiday2024_6.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
-								{ url: '/vince/Holiday2024_7.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
-								{ url: '/vince/Holiday2024_8.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
-								{ url: '/vince/Holiday2024_9.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
-								{ url: '/vince/df67fb09-a602-4dd1-966f-e27623f76aab (1).mp4' , alt: '', title: 'title', photographer: '', type: 'video'},
-								{ url: '/vince/f2fbb8b4-940c-4b6c-9b7e-47cec6094ce4 (1).mp4', alt: '', title: 'title', photographer: '', type: 'video'},
-
+									content: [
+		
+										{ url: '/vince/VinceHoliday_Video_2.mp4', alt: '', title: 'title', photographer: '', type: 'video'},
+										{ url: '/vince/Holiday2024_1.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_2.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_3.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_4.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_5.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_6.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_7.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_8.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/Holiday2024_9.1.jpg', alt: '', title: 'title', photographer: '', type: 'image'},
+										{ url: '/vince/df67fb09-a602-4dd1-966f-e27623f76aab (1).mp4' , alt: '', title: 'title', photographer: '', type: 'video'},
+										{ url: '/vince/f2fbb8b4-940c-4b6c-9b7e-47cec6094ce4 (1).mp4', alt: '', title: 'title', photographer: '', type: 'video'},
+									]
+								}
 							]
+
 						}
 					}
 					return fetch(generateApiUrl("work"), {
